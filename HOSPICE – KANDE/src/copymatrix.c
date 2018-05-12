25c7df67a9573837f540881377d43a91eb86fdce
DEZ <dese_kande@hotmail.fr>
2018-05-13 01:47:08 +0200

    copymatrix correction

--- HOSPICE – KANDE/src/copymatrix.c
+++ HOSPICE – KANDE/src/copymatrix.c
@@ -6,17 +6,49 @@
 #include "sched.h"
 
 
-struct copymatrix_args{
-    int **src;
-    int **dst;
-    int n;
-    int m;
-    int curs;
-    int index;
+typedef matrix Matrix;
+struct matrix{
+    int nrow;
+    int ncol;
+    int **data;
 };
 
-struct copymatrix_args * new_args(int **src,int **dst,int n,
-                                  int m,int curs){
+
+struct copymatrix_args{
+    Matrix *src;
+    Matrix *dst;
+    int task_index;
+    int current_ncol;
+};
+
+void create_matrix(Matrix *m,int nrow,int ncol){
+    int i;
+    n->nrow = nrow;
+    n->ncol = ncol;
+    m->data = malloc(nrow*sizeof(int*));
+    for(i = 0; i < ncol;i++){
+        m->data[i] = calloc(ncol,sizeof(int));
+    }
+}
+
+void init_matrix(Matrix *m){
+    int i,j;
+    for(i = 0; i < m->nrow;i++){
+        for(int j = 0; j < m->ncol;j++){
+            m->data[i][j] = rand()%10+1;
+        }
+    }
+}
+
+void free_matrix(Matrix *m){
+    int i;
+    for(i = 0; i < m->nrow; i++){
+        free(m->data[i]);
+    }
+    free(m);
+}
+
+struct copymatrix_args * new_args(Matrix *src,Matrix *dst,int current_ncol){
 
     struct copymatrix_args *args = malloc(sizeof(struct copymatrix_args));
     
@@ -25,49 +57,46 @@ struct copymatrix_args * new_args(int **src,int **dst,int n,
 
     args->src = src;
     args->dst = dst;
-    args->n = n;
-    args->m = m;
-    args->curs = curs;
-    args->index = curs-1;
+    args->task_index = current_ncol-1;
+    args->current_ncol = current_ncol;
 
     return args;
 }
 
-void copymatrix_serial(int **src,int **dst,int n,int m){
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m;j++){
-            dst[i][j] = src[i][j];
+void copymatrix_serial(Matrix *src,Matrix *dst){
+    for(int i = 0; i < src->nrow; i++){
+        for(int j = 0; j < src->ncol;j++){
+            dst->data[i][j] = src->data[i][j];
         }
     }
 }
+void copymatrix(void *closure, struct scheduler *s);
 
-void copymatrix(void *closure, struct scheduler *s){
-    struct copymatrix_args *args = (struct copymatrix_args *)closure;
-    int **src = args->src;
-    int **dst = args->dst;
-    int n = args->n;
-    int m = args->m;
-    int index = args->index;
-    int curs = args->curs;
+void gentask(struct copymatrix_args *args){
     int rc;
-    for(int i = 0; i < m ;i++){
-        dst[index][i] = src[index][i];
-    }
-
-    free(closure);
-
-    curs += 1;
-    
-    if(n < curs)
+    args->current_ncol += 1;    
+    if(args->src->ncol < args->current_ncol)    
         return;
-
-    rc = sched_spawn(copymatrix,new_args(src,dst,n,m,curs),s);
+    rc = sched_spawn(copymatrix,new_args(args->src,args->dst,args->current_ncol),s);
     assert(rc >= 0);
 }
 
+
+void copymatrix(void *closure, struct scheduler *s){
+    struct copymatrix_args *args = (struct copymatrix_args *)closure;
+    int i;
+    
+    for(i = 0; i < args->src->ncol;i++){
+        dst[args->task_index][i] = src[args->task_index][i];
+    } 
+    free(closure);
+    gentask(args);
+    gentask(args);
+    gentask(args);
+}
+
 int main(int argc, char **argv){
-    int **src;
-    int **dst;
+    Matrix src,dst;
     
     struct timespec begin, end;
     double delay;
@@ -104,56 +133,35 @@ int main(int argc, char **argv){
         }
     }
 
-    src = malloc(n * sizeof(*src));
-    dst = malloc(n * sizeof(*dst));
-    
-    if(src == NULL || dst == NULL)
-        exit(0);
-    
-    for(int i = 0; i < n; i++){
-        src[i] = malloc(m*sizeof(*(src[i])));
-        dst[i] =  malloc(m*sizeof(*(dst[i])));
-        
-        if(src == NULL || dst == NULL)
-            exit(0);
-        
-        for(int j = 0; j < m;j++){
-            dst[i][j] = 0;
-            if(j % 2 == 0)
-                src[i][j] = j;
-            else
-                src[i][j] = -j;
-        }
-    }
+    create_matrix(&src,n,m);
+    create_matrix(&dst,n,m);
+     
+    init_matrix(&src);
 
     clock_gettime(CLOCK_MONOTONIC, &begin);
 
     if(serial) {
-        copymatrix_serial(src,dst,n,m);
+        copymatrix_serial(&src,&src);
     } else {
         rc = sched_init(nthreads, (n + 127) / 128,
-                        copymatrix,new_args(src,dst,n,m,1));
+                        copymatrix,new_args(&src,&dst,1));
         assert(rc >= 0);
     }
 
     clock_gettime(CLOCK_MONOTONIC, &end);
     delay = end.tv_sec + end.tv_nsec / 1000000000.0 -
         (begin.tv_sec + begin.tv_nsec / 1000000000.0);
+    
     printf("Done in %lf seconds.\n", delay);
-
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            assert(src[i][j] == dst[i][j]);
+    for(int i = 0; i < src.nrow; i++){
+        for(int j = 0; j < src.ncol; j++){
+            assert(src.data[i][j] == dst.data[i][j]);
         }
     }
+    
+    free_matrix(&src);
+    free_matrix(&dst);
 
-    for(int i = 0; i < n; i++){
-        free(src[i]);
-        free(dst[i]);
-    }
-
-    free(src);
-    free(dst);
     return 0;
 
  usage:
